770fa720bfe1a1c59be5913f449507cdeac61fe0
MatheusFigueiredo-exegit <abtheus@gmail.com>
2026-03-19 21:53:14 -0300

    Teste

--- app/src/main/java/com/example/cadastrousuario/CreateUser.java
+++ app/src/main/java/com/example/cadastrousuario/CreateUser.java
@@ -5,6 +5,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 
+// Teste
 public class CreateUser extends AppCompatActivity {
     TextInputEditText editNome, editEmail;
     Button btnSalvar;
