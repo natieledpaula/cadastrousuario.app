7cfb68106217039b6ecd4ea90774b80fac39daef
MatheusFigueiredo-exegit <abtheus@gmail.com>
2026-03-19 21:29:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cadastrousuario/CreateUser.java
@@ -0,0 +1,11 @@
+package com.example.cadastrousuario;
+
+//Bloco de importaçoes
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class CreateUser extends AppCompatActivity {
+    TextInputEditText editNome, editEmail;
+    Button btnSalvar;
+}
