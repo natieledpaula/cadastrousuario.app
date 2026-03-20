96f6f4c48a425e0400170cf78fe07f2a9f53e11e
Natiele <natiele887@gmail.com>
2026-03-19 22:12:34 -0300

    Adicionando função de criar usuario com nome e email

--- app/src/main/java/com/example/cadastrousuario/CreateUser.java
+++ app/src/main/java/com/example/cadastrousuario/CreateUser.java
@@ -1,6 +1,7 @@
 package com.example.cadastrousuario;
 
 //Bloco de importaçoes
+import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
@@ -11,4 +12,16 @@ public class CreateUser extends AppCompatActivity {
     Button btnSalvar;
 
     @Override
-    protected a2
+    protected void onCreate(Bundle saveInstanceState) {
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnSalvar.setOnClickListener(v -> {
+            String nome = editNome.getText().toString();
+            String email = editEmail.getText().toString();
+
+            if(!nome.isEmpty()) {
+                MainActivity.listaNomes.add(nome + "("+email+")");
+                finish();
+            }
+        });
+    }
+}
